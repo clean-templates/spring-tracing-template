97114dbb4198af3c91447c88fe47e3a0a50600fa
RolandSall <roland.salloum00@outlook.com>
2024-02-18 11:15:03 +0200

    feat: add prometheus grafana module

---
+++ prometheus-grafana/src/test/java/org/rolandsall/prometheusgrafana/PrometheusGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rolandsall.prometheusgrafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
