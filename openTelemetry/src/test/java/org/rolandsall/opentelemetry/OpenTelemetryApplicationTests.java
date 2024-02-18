7ab8cd6f13d1616b983fa20c45d9c98aab44127d
RolandSall <roland.salloum00@outlook.com>
2024-02-18 12:16:04 +0200

    feat: add open telemetry module

---
+++ openTelemetry/src/test/java/org/rolandsall/opentelemetry/OpenTelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rolandsall.opentelemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenTelemetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
