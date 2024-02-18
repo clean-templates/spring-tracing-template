47d6914ddf58d8e547792e209facccda3a09c386
RolandSall <roland.salloum00@outlook.com>
2024-02-18 11:06:27 +0200

    first commit

---
+++ src/main/java/org/rolandsall/springtracingtemplate/SpringTracingTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.rolandsall.springtracingtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTracingTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTracingTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rolandsall/springtracingtemplate/SpringTracingTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rolandsall.springtracingtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTracingTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
