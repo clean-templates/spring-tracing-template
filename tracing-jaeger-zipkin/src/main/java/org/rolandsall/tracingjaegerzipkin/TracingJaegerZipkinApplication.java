9fa22e69df1ca721002f96b482bd0979ddfe1fc8
RolandSall <roland.salloum00@outlook.com>
2024-02-18 13:07:28 +0200

    feat: initial zipkin and jaeger tracing module

---
+++ tracing-jaeger-zipkin/src/main/java/org/rolandsall/tracingjaegerzipkin/TracingJaegerZipkinApplication.java
@@ -0,0 +1,13 @@
+package org.rolandsall.tracingjaegerzipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracingJaegerZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracingJaegerZipkinApplication.class, args);
+	}
+
+}
---
+++ tracing-jaeger-zipkin/src/test/java/org/rolandsall/tracingjaegerzipkin/TracingJaegerZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rolandsall.tracingjaegerzipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracingJaegerZipkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
